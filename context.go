f17b067bab0be6c4a53d65ce3f851a0bff4258f6
gernest <geofreyernest@live.com>
2016-10-01 11:51:00 +0300

    Add the Contextrenderer
    This will replace the Default render eventually

--- context.go
+++ context.go
@@ -1,6 +1,14 @@
 package bongo
 
-import "sort"
+import (
+	"encoding/json"
+	"html/template"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+)
 
 //Context contains the tree of processed pages.
 type Context struct {
@@ -8,6 +16,14 @@ type Context struct {
 	Tags    TagList
 	Untaged PageList
 	Data    map[string]interface{}
+}
+
+type Config struct {
+	ThemeDir     string
+	Theme        string
+	DataDir      string
+	TemplatesDir string
+	OutputDir    string
 }
 
 //GetAllSections retruns a Context object for the PageList. Thismakes surepages
@@ -34,4 +50,83 @@ func GetContext(p PageList) *Context {
 		}
 	}
 	return ctx
+}
+
+type ContextRender struct {
+	ctx  *Context
+	cfg  *Config
+	tpl  *Template
+	root string
+}
+
+func (r *ContextRender) Before(root string) error {
+	configPath := filepath.Join(root, DefaultConfigFile)
+	b, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		return nil
+	}
+	cfg := &Config{}
+	err = json.Unmarshal(b, cfg)
+	if err != nil {
+		return nil
+	}
+	r.cfg = cfg
+	r.root = root
+	tpl, err := LoadTemplates(root, cfg)
+	if err != nil {
+		return nil
+	}
+	r.tpl = tpl
+	return nil
+}
+
+type Template struct {
+	Theme *template.Template
+	Main  *template.Template
+}
+
+func LoadTemplates(base string, cfg *Config) (*Template, error) {
+	t := &Template{
+		Theme: template.New("theme"),
+		Main:  template.New("main"),
+	}
+	err := loadTpl(filepath.Join(base, cfg.ThemeDir, cfg.Theme), t.Theme)
+	if err != nil {
+		return nil, err
+	}
+	err = loadTpl(filepath.Join(base, cfg.TemplatesDir), t.Main)
+	if err != nil {
+		return nil, err
+	}
+	return t, nil
+}
+
+func loadTpl(base string, tpl *template.Template) error {
+	return filepath.Walk(base, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() || !HasExt(path, defalutTplExtensions...) {
+			return nil
+		}
+		b, err := ioutil.ReadFile(path)
+		if err != nil {
+			return err
+		}
+		n := strings.TrimPrefix(path, base)
+		tx := tpl.New(n[1:])
+		_, err = tx.Parse(string(b))
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+
+}
+
+func (t *Template) Lookup(name string) *template.Template {
+	if theme := t.Theme.Lookup(name); theme != nil {
+		return theme
+	}
+	return t.Main.Lookup(name)
 }
